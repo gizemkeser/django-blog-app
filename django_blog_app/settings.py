98d63455149ae508253afdeaa2550c70e96bf39e
gizemkeser <gzemkeser@gmail.com>
2019-02-04 00:58:03 +0300

    Python decouple added

--- django_blog_app/settings.py
+++ django_blog_app/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8le%%)07%n!*%hbt&e2e-v1ocy66qk%ab(1p9=o0+#ld#-foep'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
